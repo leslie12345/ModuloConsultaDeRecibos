2d9f8fe3e89f95f956ed0adefeb5385479634e78
leslie12345 <leslie.tovar@unmsm.edu.pe>
2019-10-18 16:04:07 -0500

    Heroku deploy.

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.js'));
 });app.listen(port);
